dcf6f4313eedacf84187e6e3c729da0c56fb8bbc
LH <540864802@qq.com>
2021-07-05 10:47:08 +0800

    添加左滑模块

--- Swipeable/Swipeable/ViewController.swift
+++ Swipeable/Swipeable/ViewController.swift
@@ -20,16 +20,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         table = UITableView(frame: self.view.bounds)
-//        table.setEditing(true, animated: true)
+        //        table.setEditing(true, animated: true)
         self.view .addSubview(table)
         table.delegate = self
         table.dataSource = self
-//        table.separatorStyle = .none
+        //        table.separatorStyle = .none
         dataSource = lyric.split(separator: ",").map(String.init)
         actionController = UIAlertController(title: "", message: "", preferredStyle: UIAlertController.Style.alert)
         
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataSource.count
     }
@@ -38,10 +38,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
         cell.textLabel?.text = self.dataSource[indexPath.row]
         cell.selectionStyle = .none;
-        cell.textLabel?.textColor = UIColor.black
-
+        cell.textLabel?.textColor = UIColor.black        
         return cell
-    
+        
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
@@ -62,12 +61,59 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             dataSource.remove(at: indexPath.row)
             // 刷新
             tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.bottom)
-                
+            
         }
-       
+        
     }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let more = UIContextualAction(style: .destructive, title: "编辑") {
+            (action, view, completionHandler) in
+            //.......
+            let alertController = UIAlertController(title: "系统提示",
+                                                    message: "您确定要编辑吗？", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+            let okAction = UIAlertAction(title: "好的", style: .default, handler: {
+                action in
+                print("点击了确定")
+            })
+            alertController.addAction(cancelAction)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+            completionHandler(true)
+        }
+        more.backgroundColor = UIColor .green
+        //创建“旗标”事件按钮
+        let favorite = UIContextualAction(style: .normal, title: "旗标") {
+            (action, view, completionHandler) in
+            //.......
+            completionHandler(true)
+        }
+        favorite.backgroundColor = UIColor .blue
+        //创建“删除”事件按钮
+        let delete = UIContextualAction(style: .destructive, title: "删除") {
+            (action, view, completionHandler) in
+            //.......
+            completionHandler(true)
+        }
+        
+        //返回所有的事件按钮
+        let configuration = UISwipeActionsConfiguration(actions: [delete, favorite, more])
+        configuration.performsFirstActionWithFullSwipe = false
+        return configuration
+    }
+    
+    
+    
+    
     @IBAction func userDidEditBtn(_ sender: UIBarButtonItem) {
         table.setEditing(!table.isEditing, animated: true)
     }
+    
+    
+    
+    
+    
 }
 
